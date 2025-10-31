ceaf503aa3564525e28e6cb0885fdb5b96e4fa48
HemantS1609 <hemantsonar1609@gmail.com>
2025-10-31 17:33:09 +0530

    Add healthcheck route and controller: implement healthcheck endpoint with status response; update app.js for route declaration.

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,7 +1,17 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
   //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  const healthStatus = {
+    status: "ok",
+    uptime: process.uptime(), // Server uptime in seconds
+    timestamp: new Date().toISOString(), // Current server time
+    message: "Server is healthy and running",
+  };
+  return res
+    .status(200)
+    .json(new ApiResponse(200, healthStatus, "Health check successful"));
 });
 
 export { healthcheck };
